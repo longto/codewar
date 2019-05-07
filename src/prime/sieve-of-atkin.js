3828d9f59ab4572e05a8b8f261b638922226c1fc
longto <long.to@niteco.se>
2019-05-07 17:25:04 +0700

    update

--- src/prime/sieve-of-atkin.js
+++ src/prime/sieve-of-atkin.js
@@ -1,34 +1,37 @@
-import {lzw_decode, lzw_encode} from './lzw';
 const MAX = 25000000;
-const prime = [];
-const bitMax = 32;
-const sieve = [...Array(Math.ceil(MAX/bitMax)).fill(0)];
+const prime = [2,3,5];
+const bit = 5;
+const sieve = [...Array((MAX>>bit)+1).fill(0)];
+const mark =    (n, i=n>>bit, j=n-(i<<bit), mask=1<<j) => sieve[i] |= mask;
+const toggle =  (n, i=n>>bit, j=n-(i<<bit), mask=1<<j) => sieve[i] ^= mask;
+const clear =   (n, i=n>>bit, j=n-(i<<bit), mask=1<<j) => sieve[i] &= ~mask;
+const check =   (n, i=n>>bit, j=n-(i<<bit), tmp=sieve[i]) => (tmp>>j)&1;
+const mod60 =   a => a%60;
 
-const mark = (n, i=n/bitMax, j=n%bitMax, mask=1<<j) => sieve[i] |= mask;
-const toggle = (n, i=n/bitMax, j=n%bitMax, mask=1<<j) => sieve[i] ^= mask;
-const clear = (n, i=n/bitMax, j=n%bitMax, mask=1<<j) => sieve[i] &= ~mask;
-const check = (n, i=n/bitMax, j=n%bitMax, tmp=sieve[i]) => (tmp>>j)&1;
-
-const view = (n=MAX, result = [2,3]) => {
-     for( let k=5; k<n; k++) {
-        if (check(k)) {
-            result.push(k);
-        }
-    }
-    console.log(lzw_encode(result.join(',')));
+const view = () => {
+    console.log(prime.join(','));
 }
-
-const screening = ( max, sqrtMax=Math.sqrt(max), n) => {
+const mod1 = [1, 13, 17, 29, 37, 41, 49, 53];
+const mod2 = [7, 19, 31, 43];
+const mod3 = [11, 23, 47, 59];
+const screening = ( max, sqrtMax=Math.sqrt(max), n, m) => {
     for ( let x=1; x<=sqrtMax; x++ ) {
         for ( let y=1; y<=sqrtMax; y++ ) {
-            if ( (n=4*x*x + y*y) < max && ( n%12===1 || n%12===5) ) {
-                mark(n);
+            n = 4*x*x + y*y; 
+            m = mod60(n)
+            ;
+            if ( n < max && mod1.includes(m) ) {
+                toggle(n);
             }
-            if ( (n=3*x*x + y*y) < max && n%12===7 ) {
-                mark(n);
+            n = 3*x*x + y*y; 
+            m = mod60(n);
+            if ( n < max && mod2.includes(m) ) {
+                toggle(n);
             }
-            if ( (n=3*x*x - y*y) < max && n%12===11 ) {
-                mark(n);
+            n = 3*x*x - y*y; 
+            m = mod60(n);
+            if ( n < max && mod3.includes(m) ) {
+                toggle(n);
             }
         }
     }
@@ -39,6 +42,11 @@ const screening = ( max, sqrtMax=Math.sqrt(max), n) => {
             }
         }
     }
+    for( let k=5; k<n; k++) {
+        if (check(k)) {
+            prime.push(k);
+        }
+    }
     view();
 }
 
@@ -46,7 +54,20 @@ const main = () => {
     let t1 = (new Date).getTime();
     screening(MAX);
     let t2 = (new Date).getTime();
-    console.log(`time to run : ${(t2-t1)/1000} s`)
+    console.log(`time to run : ${(t2-t1)/1000} s`);
 }
 
 export {main};
+
+// const mod3 = a => {
+//     a = (a >> 16) + (a & 0xFFFF); 
+//     a = (a >>  8) + (a & 0xFF);
+//     a = (a >>  4) + (a & 0xF);
+//     a = (a >>  2) + (a & 0x3);
+//     a = (a >>  2) + (a & 0x3);
+//     a = (a >>  2) + (a & 0x3);
+//     return a > 2 ? a - 3 : a;
+// }
+// const mod4 = a => a&3;
+// const mod12Table = [[0,4,8],[9,1,5],[6,10,2],[3,7,11]];
+//const mod12 = a => mod12Table[mod4(a)][mod3(a)];
